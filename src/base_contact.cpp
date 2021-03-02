dfc35c9345257be046adcd95b64596612ba8237b
Nam <lkunelk@gmail.com>
2021-03-01 23:44:35 -0800

    Use CLion's automatic code refactoring
    Refactoring function will help to make code consistent.
    Currently just using the default code style with indent=2

--- src/base_contact.cpp
+++ src/base_contact.cpp
@@ -10,22 +10,24 @@ const std::string BASE_LINK_NAME("robot::base_link::base_link_collision");
 const std::string WORLD_LINK_NAME("ground_plane::link::collision");
 
 boost::circular_buffer<float> contact_buffer(32);
+
 bool collided_() {
   uint8_t j = 0;
-  for(bool contacted : contact_buffer) {
+  for (bool contacted : contact_buffer) {
     j += contacted;
   }
   return j > 1;
 }
+
 gazebo::transport::SubscriberPtr sub; // what. so if this isn't scoped the subscription just gets garbage-collected? Eugh. How?!
 ros::Publisher pub;
 std_msgs::Bool contact_msg;
 
 void tick(ConstContactsPtr &msg) {
-  for(uint32_t i = 0; i < msg->contact_size(); i++) {
+  for (uint32_t i = 0; i < msg->contact_size(); i++) {
     auto contact = msg->contact(i);
-    if((BASE_LINK_NAME.compare(contact.collision1()) == 0 && WORLD_LINK_NAME.compare(contact.collision2()) == 0) ||
-      (WORLD_LINK_NAME.compare(contact.collision1()) == 0 && BASE_LINK_NAME.compare(contact.collision2()) == 0)) {
+    if ((BASE_LINK_NAME.compare(contact.collision1()) == 0 && WORLD_LINK_NAME.compare(contact.collision2()) == 0) ||
+        (WORLD_LINK_NAME.compare(contact.collision1()) == 0 && BASE_LINK_NAME.compare(contact.collision2()) == 0)) {
       // collision with base link: usual physics
       contact_buffer.push_back(true);
       return;
@@ -52,16 +54,16 @@ void gazebo_init(int _argc, char **_argv) {
   sub = node->Subscribe("~/physics/contacts", contactCallback);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ros::init(argc, argv, "base_contact");
   ros::NodeHandle n;
-  
+
   pub = n.advertise<std_msgs::Bool>("base_contact", 1000);
-  
+
   gazebo_init(argc, argv);
-  
+
   ros::spin();
-  
+
   // Make sure to shut everything down.
   gazebo::client::shutdown();
   return 0;
--- src/util.cpp
+++ src/util.cpp
@@ -1,9 +1,9 @@
 #include <vector>
 #include <string>
 
-int idxOf(const std::vector<std::string> a, const std::string& b) {
-  for(int i = 0; i < a.size(); i++) {
-    if(a[i].compare(b) == 0)
+int idxOf(const std::vector <std::string> a, const std::string &b) {
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i].compare(b) == 0)
       return i;
   }
   return -1;
