2ac2e74ebace45cdf8c19d1c56ffad6fbae39faf
Nam <lkunelk@gmail.com>
2021-03-04 12:22:49 -0800

    spring k can be changed by publishing topic

--- src/spring_emulator.cpp
+++ src/spring_emulator.cpp
@@ -2,10 +2,14 @@
 #include "sensor_msgs/JointState.h"
 #include "std_msgs/Float64.h"
 
-const float SPRING_K = 385.0f; // 0.1925; // N/m
+float SPRING_K = 385.0f; // 0.1925; // N/m
 
 ros::Publisher springPub;
 std_msgs::Float64 springMsg;
+
+void cbUpdateSpringK(const std_msgs::Float64 &msg) {
+  SPRING_K = msg.data;
+}
 
 void cb(const sensor_msgs::JointState &msg) {
   springMsg.data = -SPRING_K * msg.position[1];
@@ -13,10 +17,11 @@ void cb(const sensor_msgs::JointState &msg) {
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "prism_ctrl");
+  ros::init(argc, argv, "spring_emulator");
   ros::NodeHandle n;
   springPub = n.advertise<std_msgs::Float64>("arm_spring/command", 1000);
   ros::Subscriber sub = n.subscribe("/joint_states", 1000, cb);
+  ros::Subscriber subSpringK = n.subscribe("/update_spring_k", 1000, cbUpdateSpringK);
   ros::spin();
   return 0;
 }
