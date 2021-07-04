0315835aa56afb7b8b11dcf0d5a1b4e9e65b8589
Nam <lkunelk@gmail.com>
2021-07-03 19:27:59 -0700

    get jumping command to work

--- pendulum/src/jump_control.cpp
+++ pendulum/src/jump_control.cpp
@@ -1,57 +1,53 @@
+#include <algorithm>
+#include <boost/circular_buffer.hpp>
+
+#include "consts.hpp"
+#include "gazebo_msgs/LinkStates.h"
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
-#include "gazebo_msgs/LinkStates.h"
-#include "std_msgs/Float64.h"
 #include "std_msgs/Bool.h"
+#include "std_msgs/Float64.h"
 #include "util.hpp"
-#include "consts.hpp"
 
-#include <boost/circular_buffer.hpp>
-#include <algorithm>
+const float MAX_ANGV = 9.04f;  // rad/s, 0.1s/60deg
+const float T_STALL = 2.8f;    // N-m
 
-const float MAX_ANGV = 9.04f; // rad/s, 0.1s/60deg
-const float T_STALL = 2.8f; // N-m
-
-bool last_collided = false;
-boost::circular_buffer<float> zvel_buf(32);
+bool lastTouchSensorVal = false;
+bool fireServo = false;
+ros::Time beginFireServo;
 
 ros::Publisher commandPub;
 std_msgs::Float64 commandMsg;
 
-void link_cb(const gazebo_msgs::LinkStates &msg) {
-  zvel_buf.push_back(msg.twist[idxOf(msg.name, FLY_LINK_NAME)].linear.z);
-}
+void base_contact_cb(const std_msgs::Bool msg) { lastTouchSensorVal = msg.data; }
 
-void cb(const sensor_msgs::JointState &msg) {
+// emulate simplified servo motor dynamics
+void servo_emulation_cb(const sensor_msgs::JointState &msg) {
   int joint_idx = idxOf(msg.name, PRISM_JOINT_NAME);
   float v = msg.velocity[joint_idx];
-  float Fm = 0.0f;
 
-  bool enF = last_collided;
-  float zvel_filt = 0.0f;
-  if (!zvel_buf.full()) {
-    enF = false;
-  } else {
-    // moving average filter, consider better FIR filters
-    for (float zvel : zvel_buf) {
-      zvel_filt += zvel / zvel_buf.size();
-    }
-    enF &= zvel_filt > 0;
-  }
-
-  // Is touching ground and velocity is upwards
-  if (enF) {
+  if (fireServo) {
     commandMsg.data = T_STALL * (1.0f - fmax(0.0f, fmin(1.0f, v / MAX_ANGV)));
   } else {
-    commandMsg.data = 0.0;
+    commandMsg.data = -0.5;  // retract servo
   }
 
-  printf("%.3f\t%.3f\t%.3f\t%.3f\t%d\t%.3f\n", zvel_filt, msg.position[0], msg.position[1], v, last_collided, commandMsg.data);
   commandPub.publish(commandMsg);
 }
 
-void base_contact_cb(const std_msgs::Bool msg) {
-  last_collided = msg.data;
+void controlUpdate() {
+  // if we touched ground fire servo for set amount of time
+  if (fireServo) {
+    ros::Duration dur = ros::Time::now() - beginFireServo;
+    bool timeExpired = dur.toSec() > 0.1;
+    if (timeExpired) {
+      fireServo = false;
+    }
+  } else if (lastTouchSensorVal == true) {
+    ROS_INFO("Fire Servo!");
+    fireServo = true;
+    beginFireServo = ros::Time::now();
+  }
 }
 
 int main(int argc, char **argv) {
@@ -60,11 +56,15 @@ int main(int argc, char **argv) {
 
   commandPub = n.advertise<std_msgs::Float64>("arm_controller/command", 1000);
 
-  ros::Subscriber sub = n.subscribe("/joint_states", 1000, cb);
+  ros::Subscriber sub = n.subscribe("/joint_states", 1000, servo_emulation_cb);
   ros::Subscriber sub_base_contact = n.subscribe("base_contact", 1000, base_contact_cb);
-  ros::Subscriber sub_links = n.subscribe("/gazebo/link_states", 1000, link_cb);
 
-  ros::spin();
+  ros::Rate rate(1000);  // Control update rate (Hz)
+  while (ros::ok()) {
+    controlUpdate();
+    ros::spinOnce();  // process all the callbacks
+    rate.sleep();
+  }
 
   return 0;
 }
