34b5ef9d9f17ce340fd9ba1a5a996d73ce41e186
Nam <lkunelk@gmail.com>
2021-07-08 22:31:48 -0700

    format spring-emulator code

--- pendulum/src/spring_emulator.cpp
+++ pendulum/src/spring_emulator.cpp
@@ -1,17 +1,15 @@
+#include "consts.hpp"
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
 #include "std_msgs/Float64.h"
-#include "consts.hpp"
 #include "util.hpp"
 
-float SPRING_K = 433.1f; // 0.1925; // N/m
+float SPRING_K = 433.1f;  // 0.1925; // N/m
 
 ros::Publisher springPub;
 std_msgs::Float64 springMsg;
 
-void cbUpdateSpringK(const std_msgs::Float64 &msg) {
-  SPRING_K = msg.data;
-}
+void cbUpdateSpringK(const std_msgs::Float64 &msg) { SPRING_K = msg.data; }
 
 void cb(const sensor_msgs::JointState &msg) {
   springMsg.data = -SPRING_K * msg.position[idxOf(msg.name, SPRING_JOINT_NAME)];
@@ -22,8 +20,8 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "spring_emulator");
   ros::NodeHandle n;
   springPub = n.advertise<std_msgs::Float64>("arm_spring/command", 1000);
-  ros::Subscriber sub = n.subscribe("/joint_states", 1000, cb);
-  ros::Subscriber subSpringK = n.subscribe("/update_spring_k", 1000, cbUpdateSpringK);
+  ros::Subscriber sub = n.subscribe("joint_states", 1000, cb);
+  ros::Subscriber subSpringK = n.subscribe("update_spring_k", 1000, cbUpdateSpringK);
   ros::spin();
   return 0;
 }
